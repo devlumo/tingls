73e27b7687585e4c7431a5621429172e5c74cef5
Luke Molloy <developerlumo@gmail.com>
2021-11-24 11:18:51 +0000

    installed mongoose and connected to mongodb atlas cluster

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 import "./utils/envConfig.js";
 import app from "./app.js";
 
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO_DB_URI).then(() => {
+  console.log("Connected to DB");
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
